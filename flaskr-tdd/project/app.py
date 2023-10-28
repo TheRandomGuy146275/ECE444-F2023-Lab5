0d54a7af725c1e1c51b1f325d921cefc2b552152
Marvin <marvin.wu@mail.utoronto.ca>
2023-10-28 15:31:49 -0400

    Finished second test

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, g
+
+# connect to database
+def connect_db():
+    """Connects to the database."""
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
-# create and initialize a new Flask app
-app = Flask(__name__)
+# create the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+# open database connection
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 
-if __name__ == "__main__":
-    app.run()
+# close database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
